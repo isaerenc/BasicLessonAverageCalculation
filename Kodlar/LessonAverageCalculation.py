5082d177e0ec04858a334610d3edd756b6f59d26
isaerenc <cagatayisaeren@gmail.com>
2022-02-28 18:00:07 +0300

    LessonAverageCalculation
    Bu sefer C yerine Python ile ders ortalamanızı hesaplayan bir kod bütünü.

---
+++ Kodlar/LessonAverageCalculation.py
@@ -0,0 +1,7 @@
+__author__ = "isaerenc"
+
+ders1 = float(input("Ders notunuzu giriniz:"))
+ders2 = float(input("Ders notunuzu giriniz:"))
+dersici1 = float(input("Sozlu notunuzu giriniz:"))
+dersici2 = float(input("Sozlu notunuzu giriniz:"))
+print("Dersinizin ortalaması:", (ders1+ders2+dersici1+dersici2) / 4)
